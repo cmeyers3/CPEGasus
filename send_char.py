dd256e3b5d20ad784266b4cb5f5fd3d1b7c4cbfe
cmeyers3 <cmeyers3@nd.edu>
2020-09-07 15:12:01 -0400

    Adding loop

--- send_char.py
+++ send_char.py
@@ -1,13 +1,14 @@
 import serial
+import time
 
 def main():
-    
-    print("hi") 
     ser = serial.Serial('/dev/ttyUSB0')
     print(ser.name)
     
-    ser.write(b'Hello')
-    ser.close()
+    while True:
+        time.sleep(1)
+        ser.write(b'Hello')
+        ser.close()
 
 
 if __name__ == '__main__':
